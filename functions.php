12fd7ae2740a7560bdd885858e2e46ee4763d31c
Daniel Bachhuber <d@danielbachhuber.com>
2013-04-27 10:44:51 -0700

    Track visitors with Google Analytics

--- functions.php
+++ functions.php
@@ -33,3 +33,24 @@ function td_assign_category_from_post_format( $post_id ) {
 
 	wp_set_object_terms( $post_id, array( $category ), 'category' );
 }
+
+/**
+ * Tin-foil hat: off
+ */
+add_action( 'wp_footer', 'td_wp_footer_google_analytics' );
+function td_wp_footer_google_analytics () {
+	// Don't log me!
+	if ( is_user_logged_in() )
+		return;
+	?>
+<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-40493114-1', 'danielbachhuber.com');
+	ga('send', 'pageview');
+</script>
+<?php
+}
